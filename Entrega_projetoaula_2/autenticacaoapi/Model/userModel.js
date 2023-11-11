98f845069f64f8f1f8af61916bc91c35d004d6d3
JosePedro Gomes <JosePedro Gomes@ISMJ28>
2023-11-11 13:45:50 +0000

    apagar ficheiros desnecessarios

--- Entrega_projetoaula_2/autenticacaoapi/Model/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const UserSchema= new Schema(
-    {username: {type:String,require:true,unique:true},
-    email: {type:String,require:true},
-    password: {type:String,require:true}},
-    {collection:'users'})
-const User=mongoose.model('User',UserSchema)
-module.exports=User
